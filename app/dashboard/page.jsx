0d41d64d6acbc7da7ae64873dae0005496080bd2
ramrachai <ramrachaim@gmail.com>
2024-03-31 16:18:19 +0600

    added users component

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import styles from './dashboard.module.scss';
+import { cards } from '@/lib/cardsData';
+import Card from '@/components/dashboard/card/Card';
+import Chart from '@/components/dashboard/chart/Chart';
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  console.log(cards);
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.main}>
+        <div className={styles.cards}>
+          {cards.map((item, index) => (
+            <Card item={item} key={index} />
+          ))}
+        </div>
+        <Chart />
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
