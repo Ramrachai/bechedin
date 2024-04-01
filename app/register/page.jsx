6d9427f15938671b8a51f716c739e81911d14e48
ramrachai <ramrachaim@gmail.com>
2024-04-02 02:21:41 +0600

    adding user registraion functionality

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -2,7 +2,22 @@ import Link from 'next/link';
 import React from 'react';
 import styles from './register.module.scss';
 
-const Register = () => {
+const Register = async () => {
+  async function handleSubmit(formData) {
+    'use server';
+    let name = formData.get('name');
+    const email = formData.get('email');
+    let phone = formData.get('phone');
+    let password = formData.get('password');
+
+    const res = await fetch(process.env.BASE_URL, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name, email, phone, password }),
+    });
+    const data = await res.json();
+    console.log('my data ==', data);
+  }
   return (
     <div className={styles.pageContainer}>
       <div className={styles.formContainer}>
@@ -10,17 +25,17 @@ const Register = () => {
         <p className={styles.subTitle}>
           Sign up now and get full access to Bike Arot
         </p>
-        <form className={styles.form}>
+        <form className={styles.form} action={handleSubmit}>
           <div className={styles.inputGroup}>
             <label htmlFor='fullName'>Full Name</label>
             <input
               type='text'
-              name='fullName'
+              name='name'
               id='fullName'
               placeholder='Enter your Full name'
             />
           </div>
-          {/* <div className={styles.inputGroup}>
+          <div className={styles.inputGroup}>
             <label htmlFor='phone'>
               Phone Number <span>(otp will be sent for verification)</span>
             </label>
@@ -30,7 +45,7 @@ const Register = () => {
               id='phone'
               placeholder='Enter your phone number'
             />
-          </div> */}
+          </div>
 
           <div className={styles.inputGroup}>
             <label htmlFor='email'>Email</label>
@@ -41,7 +56,7 @@ const Register = () => {
               placeholder='Enter your Email'
             />
           </div>
-
+          {/* 
           <div className={styles.inputGroup}>
             <label htmlFor='address'>Address</label>
             <input
@@ -50,7 +65,7 @@ const Register = () => {
               id='address'
               placeholder='Enter your full address'
             />
-          </div>
+          </div> */}
 
           <div className='flex gap-4'>
             <div className={styles.inputGroup}>
